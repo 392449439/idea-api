8e911989a15041ba184e03d8bd2c384143430f59
李传浩 <ALNY-AL@outlook.com>
2019-10-17 13:48:38 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+
+    public function boot(UrlGenerator $url)
     {
         //
+        if (env('REDIRECT_HTTPS')) {
+            $url->forceSchema('https');
+        }
     }
 }
