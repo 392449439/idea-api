d63d8f8ff368414eaec23b72c44f4ea6d07318ae
lizilong <35825874+liyijieac@users.noreply.github.com>
2019-10-21 15:19:53 +0800

    delete testController
    删除testcontroller

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/test', 'TestController@test');
+// $router->get('/test', 'TestController@test');
 
 $router->group(['namespace' => 'Auth', 'prefix' => 'auth'], function () use ($router) {
     $router->post('login', 'AuthController@login');
