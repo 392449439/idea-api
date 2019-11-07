49f4af089fbbe777e90a855c313dd8cc4935387c
李传浩 <ALNY-AL@outlook.com>
2019-11-07 17:10:03 +0800

    Update AuthController.php

--- app/Http/Controllers/Mini/Auth/AuthController.php
+++ app/Http/Controllers/Mini/Auth/AuthController.php
@@ -47,6 +47,7 @@ class AuthController extends Controller
 			'response_type' => 'array',
 		]);
 		$res = $app->auth->session($request->input('code'));
+		return $res;
 		$decryptedData = $app->encryptor->decryptData($res['session_key'], $request->input('iv'), $request->input('encryptedData'));
 		return $decryptedData;
 
