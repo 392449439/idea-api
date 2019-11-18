7662a20f45adad1a0773c73a02e0dea25e4321ce
李传浩 <ALNY-AL@outlook.com>
2019-11-18 18:29:59 +0800

    Update PayController.php

--- app/Http/Controllers/Mini/Order/PayController.php
+++ app/Http/Controllers/Mini/Order/PayController.php
@@ -34,10 +34,11 @@ class PayController extends Controller
 		$jssdk = $app->jssdk;
 
 		$payInfo = DB::table('pay')->where('pay_id', $request->input('pay_id'))->first();
+		// return [floatval($payInfo->price)];
 		$result = $app->order->unify([
 			'body' => '益火吃货-下单',
 			'out_trade_no' => $payInfo->pay_id,
-			'total_fee' => $payInfo->price * 1,
+			'total_fee' => floatval($payInfo->price),
 			// 'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
 			'trade_type' => 'JSAPI', // 请对应换成你的支付方式对应的值类型
 			// 'openid' =>  $payInfo->price,
