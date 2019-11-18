4d1857144699d4c4882866c26b92b9c8f2f04b8a
李传浩 <ALNY-AL@outlook.com>
2019-11-18 18:24:00 +0800

    Update PayController.php

--- app/Http/Controllers/Mini/Order/PayController.php
+++ app/Http/Controllers/Mini/Order/PayController.php
@@ -18,27 +18,26 @@ class PayController extends Controller
 
 
 
-		$config = [
-			// 必要配置
-			'app_id'             => 'wx9f4a9bdc95bcc3d7',
-			'mch_id'             => '1563112131',
-			'key'                => 'ZU30SEgmNbrmQdFNDR7gZZCF6uHLGDwC',   // API 密钥
-			// 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
-			// 'cert_path'          => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
-			// 'key_path'           => 'path/to/your/key',      // XXX: 绝对路径！！！！
+		// $config = [
+		// 	// 必要配置
+		// 	'app_id'             => 'wx9f4a9bdc95bcc3d7',
+		// 	'mch_id'             => '1563112131',
+		// 	'key'                => 'ZU30SEgmNbrmQdFNDR7gZZCF6uHLGDwC',   // API 密钥
+		// 	// 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
+		// 	// 'cert_path'          => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
+		// 	// 'key_path'           => 'path/to/your/key',      // XXX: 绝对路径！！！！
 
-			'notify_url'         => url('pay/wx_notify_url'),     // 你也可以在下单时单独设置来想覆盖它
-		];
+		// 	'notify_url'         => url('pay/wx_notify_url'),     // 你也可以在下单时单独设置来想覆盖它
+		// ];
 
-		$app = Factory::payment($config);
-		$jssdk = $app->jssdk;
+		// $app = Factory::payment($config);
+		// $jssdk = $app->jssdk;
 
 		$payInfo = DB::table('pay')->where('pay_id', $request->input('pay_id'))->first();
-
 		$result = $app->order->unify([
 			'body' => '益火吃货-下单',
 			'out_trade_no' => $payInfo->pay_id,
-			'total_fee' => $payInfo->price,
+			'total_fee' => $payInfo->price * 1,
 			// 'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
 			'trade_type' => 'JSAPI', // 请对应换成你的支付方式对应的值类型
 			// 'openid' =>  $payInfo->price,
