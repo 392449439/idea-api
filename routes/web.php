80b2ec11f595099983c985c80fbceaeef13e2d64
李传浩 <ALNY-AL@outlook.com>
2019-11-26 15:08:59 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,10 @@ $router->group(['namespace' => 'Auth', 'prefix' => 'auth'], function () use ($ro
     $router->post('login', 'AuthController@login');
 });
 
-
-$router->group(['namespace' => 'File', 'prefix' => 'file', 'middleware' => 'auth'], function () use ($router) {
-    $router->post('upload', 'FileController@upload');
+$router->group(['middleware' => 'core'], function () use ($router) {
+    $router->group(['namespace' => 'File', 'prefix' => 'file', 'middleware' => 'auth'], function () use ($router) {
+        $router->post('upload', 'FileController@upload');
+    });
 });
 
 
