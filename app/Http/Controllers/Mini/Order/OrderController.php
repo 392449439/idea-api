42af0039b5f2835b1b83f1b37fc0c44e16da5396
李传浩 <ALNY-AL@outlook.com>
2019-12-05 13:54:50 +0800

    Update OrderController.php

--- app/Http/Controllers/Mini/Order/OrderController.php
+++ app/Http/Controllers/Mini/Order/OrderController.php
@@ -16,6 +16,8 @@ class OrderController extends Controller
 	{
 
 		$source_id = $request->domainInfo->dada_source_id;
+		$is_open_dada = $request->domainInfo->is_dada === 1;
+
 		/**
 		 * 组成模型
 		 */
@@ -84,57 +86,63 @@ class OrderController extends Controller
 		/**
 		 * 组成订单
 		 */
+		$queryDeliverFee = null;
+		if ($is_open_dada) {
 
-		//哒哒
-		$dada_http = new Dada([
-			"app_key" => env('DADA_APP_KEY'),
-			"app_secret" => env('DADA_APP_SECRET'),
-			"sandbox" => env('DADA_SANDBOX'),
-			"source_id" => $source_id,
-		]);
+			//哒哒
+			$dada_http = new Dada([
+				"app_key" => env('DADA_APP_KEY'),
+				"app_secret" => env('DADA_APP_SECRET'),
+				"sandbox" => env('DADA_SANDBOX'),
+				"source_id" => $source_id,
+			]);
 
-		$order_info = [];
-		$order_info['shop_no'] = $store_id;  //门店号
-		$order_info['origin_id'] = $order_id;    //第三方订单ID
-		$order_info['city_code'] = 021;    //城市的code
-		$order_info['cargo_price'] = $price;  //订单金额
-		$order_info['is_prepay'] = 0;    //是否需要垫付 1:是 0:否 (垫付订单金额，非运费)
-		$order_info['receiver_name'] = $addressInfo['contacts'];    //收货人姓名
-		$order_info['receiver_address'] = $addressInfo['address']; //收货人地址
-		$order_info['callback'] = url('dada/notify'); //回调地址
-		$order_info['receiver_lat'] = $addressInfo['x']; //维度
-		$order_info['receiver_lng'] = $addressInfo['y']; //经度
-		$order_info['receiver_phone'] = $addressInfo['phone']; //收货人手机号
-		//        $order_info['receiver_tel'] = ;//收货人电话
-		$order_info['tips'] = 0; //小费（单位：元，精确小数点后一位）
-		$order_info['info'] = $remarks; //备注
-		$order_info['cargo_type'] = 5;
-		//        $order_info['cargo_weight'] = ;订单重量（单位：Kg）
-		//        $order_info['cargo_num'] = ;订单商品数量
-		//        $order_info['invoice_title'] = ;发票抬头
-		//        $order_info['origin_mark'] = ;订单来源标示（该字段可以显示在达达app订单详情页面，只支持字母，最大长度为10）
-		//        $order_info['origin_mark_no'] = ;订单来源编号（该字段可以显示在达达app订单详情页面，支持字母和数字，最大长度为30）
-		//        $order_info['is_use_insurance'] = ;是否使用保价费
-		//        $order_info['is_finish_code_needed'] = ;收货码（0：不需要；1：需要。
-		//        $order_info['delay_publish_time'] = ;预约发单时间（预约时间unix时间戳(10位),精确到分;整10分钟为间隔，并且需要至少提前20分钟预约。）
-		//        $order_info['is_direct_delivery'] = ;是否选择直拿直送
+			$order_info = [];
+			$order_info['shop_no'] = $store_id;  //门店号
+			$order_info['origin_id'] = $order_id;    //第三方订单ID
+			$order_info['city_code'] = 021;    //城市的code
+			$order_info['cargo_price'] = $price;  //订单金额
+			$order_info['is_prepay'] = 0;    //是否需要垫付 1:是 0:否 (垫付订单金额，非运费)
+			$order_info['receiver_name'] = $addressInfo['contacts'];    //收货人姓名
+			$order_info['receiver_address'] = $addressInfo['address']; //收货人地址
+			$order_info['callback'] = url('dada/notify'); //回调地址
+			$order_info['receiver_lat'] = $addressInfo['x']; //维度
+			$order_info['receiver_lng'] = $addressInfo['y']; //经度
+			$order_info['receiver_phone'] = $addressInfo['phone']; //收货人手机号
+			//        $order_info['receiver_tel'] = ;//收货人电话
+			$order_info['tips'] = 0; //小费（单位：元，精确小数点后一位）
+			$order_info['info'] = $remarks; //备注
+			$order_info['cargo_type'] = 5;
+			//        $order_info['cargo_weight'] = ;订单重量（单位：Kg）
+			//        $order_info['cargo_num'] = ;订单商品数量
+			//        $order_info['invoice_title'] = ;发票抬头
+			//        $order_info['origin_mark'] = ;订单来源标示（该字段可以显示在达达app订单详情页面，只支持字母，最大长度为10）
+			//        $order_info['origin_mark_no'] = ;订单来源编号（该字段可以显示在达达app订单详情页面，支持字母和数字，最大长度为30）
+			//        $order_info['is_use_insurance'] = ;是否使用保价费
+			//        $order_info['is_finish_code_needed'] = ;收货码（0：不需要；1：需要。
+			//        $order_info['delay_publish_time'] = ;预约发单时间（预约时间unix时间戳(10位),精确到分;整10分钟为间隔，并且需要至少提前20分钟预约。）
+			//        $order_info['is_direct_delivery'] = ;是否选择直拿直送
 
-		//查询订单运费接口
-		$dada_http->http('/api/order/queryDeliverFee', $order_info);
-		$queryDeliverFee = $dada_http->request();
+			//查询订单运费接口
+			$dada_http->http('/api/order/queryDeliverFee', $order_info);
+			$queryDeliverFee = $dada_http->request();
 
-		Log::info('查询运费:' . json_encode($queryDeliverFee));
-		if ($queryDeliverFee['code'] !== 0) {
-			return [
-				'code' => -1,
-				'msg' => $queryDeliverFee['msg'],
-				'data' => '',
-			];
+			Log::info('查询运费:' . json_encode($queryDeliverFee));
+			if ($queryDeliverFee['code'] !== 0) {
+				return [
+					'code' => -1,
+					'msg' => $queryDeliverFee['msg'],
+					'data' => '',
+				];
+			}
 		}
 
 
+
 		$orderInfo = [];
-		$orderInfo['freight_price'] = $queryDeliverFee['result']['fee'];
+
+
+		$orderInfo['freight_price'] = $queryDeliverFee ? $queryDeliverFee['result']['fee'] : 0;
 
 		$orderInfo['order_id'] = $order_id;
 		$orderInfo['pay_id'] = $pay_id;
@@ -146,9 +154,11 @@ class OrderController extends Controller
 		$address_id = $OrderAddressDB->insertGetId($addressInfo->toArray());
 
 		$orderInfo['address_id'] = $address_id;
-		$orderInfo['delivery_no'] = $queryDeliverFee['result']['deliveryNo'];
+		$orderInfo['delivery_no'] = $queryDeliverFee ? $queryDeliverFee['result']['deliveryNo'] : '';
 
-
+		if ($queryDeliverFee) {
+			$price += $queryDeliverFee['result']['fee'];
+		}
 
 		/**
 		 * 组成支付单数据
@@ -157,7 +167,7 @@ class OrderController extends Controller
 		$payInfo = [];
 		$payInfo['pay_id'] = $pay_id;
 		$payInfo['store_id'] = $store_id;
-		//		$payInfo['price'] = $price+$queryDeliverFee['result']['fee'];
+		$payInfo['price'] = $price;
 		$payInfo['price'] = 0.01;
 		$payInfo['domain_id'] = $request->domainInfo->domain_id;
 
@@ -252,101 +262,100 @@ class OrderController extends Controller
 	{
 		Log::info('达达进入:' . json_encode($request->all()));
 		$data = $request->all();
-		DB::table('dada_notify')->insert(['info' => json_encode($data),'order_id' => $data['order_id']]);
+		DB::table('dada_notify')->insert(['info' => json_encode($data), 'order_id' => $data['order_id']]);
 
 		//达达订单状态修改
-        $order_info = DB::table('order')
-            ->select('order_id','state')
-            ->where([
-                'order_id' ,'=',$data['order_id'],
-            ])
-            ->first();
-        //达达下单后待接单状态
-        if($data['order_status'] === 1){    //商家一接单，达达待接单
-            if($order_info->state == 0){
-                DB::table('order')
-                    ->where([
-                        'order_id','=',$data['order_id']
-                    ])
-                    ->update([
-                        'state' => $data['order_status']
-                    ]);
-            }
-        }
+		$order_info = DB::table('order')
+			->select('order_id', 'state')
+			->where([
+				'order_id', '=', $data['order_id'],
+			])
+			->first();
+		//达达下单后待接单状态
+		if ($data['order_status'] === 1) {    //商家一接单，达达待接单
+			if ($order_info->state == 0) {
+				DB::table('order')
+					->where([
+						'order_id', '=', $data['order_id']
+					])
+					->update([
+						'state' => $data['order_status']
+					]);
+			}
+		}
 
-        //达达接单后待取货状态
-        if($data['order_status'] === 2){
-            if($order_info->state < 2){
-                DB::table('order')
-                    ->where([
-                        'order_id','=',$data['order_id']
-                    ])
-                    ->update([
-                        'state' => $data['order_status']
-                    ]);
-            }
-        }
+		//达达接单后待取货状态
+		if ($data['order_status'] === 2) {
+			if ($order_info->state < 2) {
+				DB::table('order')
+					->where([
+						'order_id', '=', $data['order_id']
+					])
+					->update([
+						'state' => $data['order_status']
+					]);
+			}
+		}
 
-        //达达取货后配送状态
-        if($data['order_status'] === 3){
-            if($order_info->state < 3){
-                DB::table('order')
-                    ->where([
-                        'order_id','=',$data['order_id']
-                    ])
-                    ->update([
-                        'state' => $data['order_status']
-                    ]);
-            }
-        }
+		//达达取货后配送状态
+		if ($data['order_status'] === 3) {
+			if ($order_info->state < 3) {
+				DB::table('order')
+					->where([
+						'order_id', '=', $data['order_id']
+					])
+					->update([
+						'state' => $data['order_status']
+					]);
+			}
+		}
 
-        //达达送货完成状态
-        if($data['order_status'] === 4){
-            if($order_info->state < 4){
-                DB::table('order')
-                    ->where([
-                        'order_id','=',$data['order_id']
-                    ])
-                    ->update([
-                        'state' => $data['order_status']
-                    ]);
-            }
-        }
+		//达达送货完成状态
+		if ($data['order_status'] === 4) {
+			if ($order_info->state < 4) {
+				DB::table('order')
+					->where([
+						'order_id', '=', $data['order_id']
+					])
+					->update([
+						'state' => $data['order_status']
+					]);
+			}
+		}
 
-        //达达订单取消状态
-        if($data['order_status'] === 5){
-            DB::table('order')
-                ->where([
-                    'order_id','=',$data['order_id']
-                ])
-                ->update([
-                    'state' => $data['order_status']
-                ]);
-        }
+		//达达订单取消状态
+		if ($data['order_status'] === 5) {
+			DB::table('order')
+				->where([
+					'order_id', '=', $data['order_id']
+				])
+				->update([
+					'state' => $data['order_status']
+				]);
+		}
 
 
-        //达达订单已过期，需要重新派单
-        if($data['order_status'] === 7){
-            DB::table('order')
-                ->where([
-                    'order_id','=',$data['order_id']
-                ])
-                ->update([
-                    'state' => $data['order_status']
-                ]);
-        }
+		//达达订单已过期，需要重新派单
+		if ($data['order_status'] === 7) {
+			DB::table('order')
+				->where([
+					'order_id', '=', $data['order_id']
+				])
+				->update([
+					'state' => $data['order_status']
+				]);
+		}
 
-        //达达订单异常
-        if($data['order_status'] === 1000){
-            DB::table('order')
-                ->where([
-                    'order_id','=',$data['order_id']
-                ])
-                ->update([
-                    'state' => 9
-                ]);
-        }
-
+		//达达订单异常
+		if ($data['order_status'] === 1000) {
+			DB::table('order')
+				->where([
+					'order_id', '=', $data['order_id']
+				])
+				->update([
+					'state' => 9
+				]);
+		}
 	}
 }
 
