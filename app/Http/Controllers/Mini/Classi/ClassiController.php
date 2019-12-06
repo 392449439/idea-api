f248f46e1ef09ac2bed1ea2f8b8890489a6b1e4b
lizilong <35825874+liyijieac@users.noreply.github.com>
2019-12-06 18:20:37 +0800

    update
    修改

--- app/Http/Controllers/Mini/Classi/ClassiController.php
+++ app/Http/Controllers/Mini/Classi/ClassiController.php
@@ -3,10 +3,8 @@
 namespace  App\Http\Controllers\Mini\Classi; // @todo: 这里是要生成类的命名空间
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use EasyWeChat\Factory;
 
 
 class ClassiController extends Controller
@@ -23,7 +21,7 @@ class ClassiController extends Controller
 
 		$DB = DB::table('class')
 				->where('data_state',1)
-				->orderBy('add_time', 'desc');
+				->orderBy('sort', 'desc');
 
 		if ($request->filled('store_id')) {
 			$DB->where('store_id', $request->input('store_id'));
